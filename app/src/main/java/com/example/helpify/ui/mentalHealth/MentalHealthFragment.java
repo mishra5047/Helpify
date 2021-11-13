36dd42b8510739121ddb78e55c0737a1c3831a4f
mishra5047 <mishra695047@gmail.com>
2021-11-14 00:13:37 +0530

    Fragment Set Up

--- app/src/main/java/com/example/helpify/ui/mentalHealth/MentalHealthFragment.java
+++ app/src/main/java/com/example/helpify/ui/mentalHealth/MentalHealthFragment.java
@@ -8,15 +8,16 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.helpify.databinding.FragmentMentalHealthBinding;
 import com.example.helpify.databinding.FragmentProfileBinding;
 
 public class MentalHealthFragment extends Fragment {
 
-    private FragmentProfileBinding binding;
+    private FragmentMentalHealthBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        binding = FragmentMentalHealthBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         return root;
