2e61f2c85878b6f041642eac0c81914ed4051471
mishra5047 <mishra695047@gmail.com>
2021-11-13 23:22:58 +0530

    Refactored

---
+++ app/src/main/java/com/example/helpify/ui/mentalHealth/MentalHealthFragment.java
@@ -0,0 +1,24 @@
+package com.example.helpify.ui.mentalHealth;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.helpify.databinding.FragmentProfileBinding;
+
+public class MentalHealthFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+}
